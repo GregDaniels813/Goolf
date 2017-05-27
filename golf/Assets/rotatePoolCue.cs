1f04e2f1bcb5f1efb54f22e0865ae38a635be70e
GregDaniels813 <drichman813@gmail.com>
2017-05-26 19:00:43 -0600

    Changes after watching 2nd video, cue rotation, friction, etc.

---
+++ golf/Assets/rotatePoolCue.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotatePoolCue : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if (Input.GetKeyDown("a"))
+        {
+            transform.Rotate(0, 1, 0);
+        }
+
+        if (Input.GetKeyDown("d"))
+        {
+            transform.Rotate(0, -1, 0);
+
+        }
+
+        if 
+    }
+}
